982eaad115140aa8c0b0eab87403efd723880441
kunal232i <kingawale23@gmail.com>
2023-08-02 09:14:42 +0000

    advance types

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,5 @@
 "use strict";
-let age = 12;
-let Name = "xyz";
-let published = true;
-let arr = [2, 3, 4];
-let anyArr = [];
-let any;
-let tuple = [1, "Hello"];
-var Size;
-(function (Size) {
-    Size[Size["Small"] = 1] = "Small";
-    Size[Size["Medium"] = 2] = "Medium";
-    Size[Size["Large"] = 3] = "Large";
-})(Size || (Size = {}));
-let mySize = Size.Medium;
-console.log(mySize);
-const calTax = (income, taxYear) => {
-    if (taxYear < 2022)
-        return income * 1.2;
-    return income * 1.3;
-};
-console.log(calTax(2, 2022));
+var _a;
 let employee = {
     id: 1,
     name: "xyz",
@@ -27,4 +7,34 @@ let employee = {
         console.log(date);
     },
 };
+const kgToLbs = (weight) => {
+    if (typeof weight === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+};
+console.log(kgToLbs(10));
+console.log(kgToLbs("20kg"));
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 50;
+let metric = "cm";
+const greeting = (msg) => {
+    if (msg)
+        console.log(msg);
+    else
+        console.log("hello!");
+};
+greeting("Hola!");
+greeting(null);
+greeting(undefined);
+const getCustomer = (id) => {
+    return id === 0 ? null : { birthday: new Date() };
+};
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let log = null;
+log === null || log === void 0 ? void 0 : log("a");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,74 @@
-// built in type
-
-let age = 12;
-let Name = "xyz";
-let published = true;
-let arr = [2, 3, 4];
-
-// any type
-let anyArr = [];
-let any;
-
-// Tuples -> key pair
-let tuple: [number, string] = [1, "Hello"];
-
-// Enum
-enum Size {
-  Small = 1,
-  Medium,
-  Large,
-}
-let mySize: Size = Size.Medium;
-console.log(mySize);
-
-//functions
-const calTax = (income: number, taxYear: number): number => {
-  if (taxYear < 2022) return income * 1.2;
-  return income * 1.3;
-};
-console.log(calTax(2, 2022));
-
-//objects
-let employee: {
+// type alies
+type Employee = {
   readonly id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+let employee: Employee = {
   id: 1,
   name: "xyz",
   retire: (date: Date) => {
     console.log(date);
   },
 };
+
+// union types
+const kgToLbs = (weight: number | string): number => {
+  // Narrowing
+  if (typeof weight === "number") return weight * 2.2;
+  else return parseInt(weight) * 2.2;
+};
+
+console.log(kgToLbs(10));
+console.log(kgToLbs("20kg"));
+
+// intersection types
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+// literal (exact, specific) types
+type Quantity = 50 | 100;
+let quantity: Quantity = 50;
+
+type Metric = "cm" | "inch";
+let metric: Metric = "cm";
+
+// Nullable types
+const greeting = (msg: string | null | undefined) => {
+  if (msg) console.log(msg);
+  else console.log("hello!");
+};
+greeting("Hola!");
+greeting(null);
+greeting(undefined);
+
+// optional chaining
+type Customer = {
+  birthday?: Date;
+};
+
+const getCustomer = (id: number | null): Customer | null | undefined => {
+  return id === 0 ? null : { birthday: new Date() };
+};
+// optional property access operator -> "?"
+let customer = getCustomer(1);
+console.log(customer?.birthday?.getFullYear());
+
+// opitional element access operator
+// customer?.[0]
+
+// optional call
+let log: any = null;
+log?.("a");
