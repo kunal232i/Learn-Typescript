b1a15e1f137a2f1dfeda16c9418ce0a5dad334ac
kunal232i <kingawale23@gmail.com>
2023-08-02 08:40:13 +0000

    basic types

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,30 @@
 "use strict";
-let x = 12;
-if (x < 50) {
-    x += 20;
-}
+let age = 12;
+let Name = "xyz";
+let published = true;
+let arr = [2, 3, 4];
+let anyArr = [];
+let any;
+let tuple = [1, "Hello"];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+const calTax = (income, taxYear) => {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+};
+console.log(calTax(2, 2022));
+let employee = {
+    id: 1,
+    name: "xyz",
+    retire: (date) => {
+        console.log(date);
+    },
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
-let x: number = 12;
+// built in type
 
-if (x < 50) {
-  x += 20;
+let age = 12;
+let Name = "xyz";
+let published = true;
+let arr = [2, 3, 4];
+
+// any type
+let anyArr = [];
+let any;
+
+// Tuples -> key pair
+let tuple: [number, string] = [1, "Hello"];
+
+// Enum
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
 }
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+//functions
+const calTax = (income: number, taxYear: number): number => {
+  if (taxYear < 2022) return income * 1.2;
+  return income * 1.3;
+};
+console.log(calTax(2, 2022));
+
+//objects
+let employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "xyz",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
